e585163df5af30e9b9635fe4679818e2259e8ea8
Coding Saint <codingsaint@gmail.com>
2021-01-04 17:33:17 +0530

    Coding Patterns

--- src/main/slidingwindows/MinimumWindowSubstring.java
+++ src/main/slidingwindows/MinimumWindowSubstring.java
@@ -6,7 +6,8 @@ import java.util.Map;
 public class MinimumWindowSubstring {
     public static void main(String[] args) {
         MinimumWindowSubstring minimumWindowSubstring=new MinimumWindowSubstring();
-        minimumWindowSubstring.minWindow("ADOBECODEBANC","ABC");
+        String res=minimumWindowSubstring.minWindow("ADOBECODEBANC","ABC");
+        System.out.println(res);
 
     }
 
@@ -21,17 +22,32 @@ public class MinimumWindowSubstring {
 
         int st=0;
         int count=0;
-        int minLength=s.length();
+        int subStringSt=0;
+        int minLength=s.length()+1;
         for (int end = 0; end < s.length() ; end++) {
             char current=s.charAt(end);
             if(freq.containsKey(current)){
                 freq.put(current, freq.get(current)-1);
+                if(freq.get(current)>=0)
                 count++;
             }
 
+            while(count==t.length()){
+                //Shrink The Window
+                if(end-st+1<minLength){
+                    minLength=end-st+1;
+                    subStringSt=st;
+                }
+                char charAtStart= s.charAt(st++);
+                if(freq.containsKey(charAtStart)){
+                    if(freq.get(charAtStart)==0)
+                        count--;
+                    freq.put(charAtStart, freq.get(charAtStart)+1);
 
+                }
+            }
         }
-        return minimumWindow;
+        return minLength > s.length() ? "": s.substring(subStringSt,subStringSt+ minLength) ;
 
     }
 }
