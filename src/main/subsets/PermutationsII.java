0ff8fb9f5a2e8b94b740557ec4ab02c04cdee018
Coding Saint <codingsaint@gmail.com>
2021-01-21 22:06:38 +0530

    Day 165

--- src/main/subsets/PermutationsII.java
+++ src/main/subsets/PermutationsII.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 //47. Permutations II
 public class PermutationsII {
-
+    //Gobal Variable for Way 1 using recussion
+/*Set<List<Integer>> vals= new HashSet<>();
+    int visited[]=null*/
     public List<List<Integer>> permuteUnique(int[] nums) {
        /*
        Way 1 Recurssion
@@ -13,54 +15,53 @@ public class PermutationsII {
 */
 
 
-        Set<List<Integer>> sol= new HashSet<>();
-    Queue<List<Integer>> q= new LinkedList<>();
+        Set<List<Integer>> sol = new HashSet<>();
+        Queue<List<Integer>> q = new LinkedList<>();
 
         q.add(new ArrayList<>());
-        for(int current:nums){
-        int size= q.size();
-        for(int i=0;i<size;i++){
-            List<Integer> oldPerm= q.poll();
-            for(int j=0;j<=oldPerm.size(); j++){
-                List<Integer> newPerm= new ArrayList<>(oldPerm);
-                newPerm.add(j,current );
-                if(newPerm.size()==nums.length)
-                    sol.add(newPerm);
-                else
-                    q.add(newPerm);
+        for (int current : nums) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> oldPerm = q.poll();
+                for (int j = 0; j <= oldPerm.size(); j++) {
+                    List<Integer> newPerm = new ArrayList<>(oldPerm);
+                    newPerm.add(j, current);
+                    if (newPerm.size() == nums.length)
+                        sol.add(newPerm);
+                    else
+                        q.add(newPerm);
+
+
+                }
 
 
             }
 
-
         }
+        List<List<Integer>> fsol = new ArrayList<>();
 
-    }
-    List<List<Integer>> fsol= new ArrayList<>();
-
-        for(List<Integer> s: sol){
-        fsol.add( new ArrayList<>(s));
-    }
-    //System.out.println(fsol);
+        for (List<Integer> s : sol) {
+            fsol.add(new ArrayList<>(s));
+        }
+        //System.out.println(fsol);
         return fsol;
-}
+    }
 
     private void permute(int[] nums, ArrayList<Integer> list) {
-        if(list.size()==nums.length) {
+        if (list.size() == nums.length) {
             vals.add(new ArrayList<>(list));
             return;
         }
-        for(int i=0;i<nums.length;i++) {
-            if(visited[i]==0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 0) {
                 list.add(nums[i]);
-                visited[i]=1;
+                visited[i] = 1;
                 permute(nums, list);
-                list.remove(list.size()-1);
-                visited[i]=0;
+                list.remove(list.size() - 1);
+                visited[i] = 0;
             }
 
         }
-
 
 
     }
